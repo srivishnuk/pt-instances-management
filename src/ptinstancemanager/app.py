0165e4138808112d6378b01f3cb0848a14821ee8
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-11-18 18:06:03 +0000

    Adding Celery to Flask web app.

--- src/ptinstancemanager/app.py
+++ src/ptinstancemanager/app.py
@@ -6,9 +6,24 @@ Created on 13/07/2015
 
 import logging
 from flask import Flask
+from celery import Celery
 from flask.ext.sqlalchemy import SQLAlchemy
 from flasgger import Swagger
 from ptinstancemanager.config import configuration
+
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
 
 
 # Configure logging
@@ -27,6 +42,9 @@ app.config['DOCKER_VNC_PORT'] =  configuration.get_docker_vnc_port()
 app.config['DOCKER_PT_PORT'] =  configuration.get_docker_pt_port()
 app.config['CACHE_DIR'] =  configuration.get_cache_directory()
 app.config['CACHE_CONTAINER_DIR'] =  configuration.get_container_directory()
+app.config['CELERY_BROKER_URL'] = configuration.get_celery_broker_url()
+app.config['CELERY_RESULT_BACKEND'] = configuration.get_celery_broker_url()
+app.config['CELERY_IMPORTS'] = ('ptinstancemanager.tasks',)
 app.config['SWAGGER'] = {
     "swagger_version": "2.0",
     "title": "pt-instances-management",
@@ -42,3 +60,6 @@ swagger = Swagger(app)
 
 # Configure DB
 db = SQLAlchemy(app)
+
+# Configure celery
+celery = make_celery(app)
