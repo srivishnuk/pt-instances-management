5f19ec64553530b1c39dcd0d85c60682f31470e1
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-11-18 18:02:28 +0000

    Reading config file path from environment by default and adding celery details.

--- src/ptinstancemanager/config.py
+++ src/ptinstancemanager/config.py
@@ -5,15 +5,18 @@ Created on 13/07/2015
 """
 
 import ConfigParser
+import os
 
 
 class ConfigFileReader(object):
 
     def __init__(self):
         self.config = ConfigParser.RawConfigParser()
+	self.set_file_path(os.environ.get('PTINSTANCEMNGR'))
 
     def set_file_path(self, file_path):
-        self.config.read(file_path)
+	if file_path:  # Ignore if it is None
+            self.config.read(file_path)
 
     def get_log(self):
         return self.config.get('Log', 'file')
@@ -36,6 +39,9 @@ class ConfigFileReader(object):
     def get_database_uri(self):
         return self.config.get('Database', 'uri')
 
+    def get_celery_broker_url(self):
+        return self.config.get('Celery', 'broker_url')
+
     def get_lowest_port(self):
         return int(self.config.get('Port', 'lowest'))
 
@@ -49,7 +55,6 @@ class ConfigFileReader(object):
     def get_container_directory(self):
         ret = self.config.get('CachedFiles', 'container_dir')
         return ret if ret.endswith('/') else ret + '/'
-
 
 
 configuration = ConfigFileReader()
