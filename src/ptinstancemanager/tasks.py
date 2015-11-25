973cd9de31f2e2198a00a79eb37372b8e185bb75
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-11-25 20:01:10 +0000

    Decorator inside decorator.

--- src/ptinstancemanager/tasks.py
+++ src/ptinstancemanager/tasks.py
@@ -15,23 +15,25 @@ logger = logging.getLogger()
 
 
 def cancellable(check=('cpu', 'memory')):
-    def has_enough_resources(func, *args, **kwargs):
-        """Has the machine reached the CPU consumption threshold?"""
-        if 'cpu' in check:
-            max_cpu = app.config['MAXIMUM_CPU']
-            current = psutil.cpu_percent(interval=1)  #  It blocks it for a second
-            if current >= max_cpu:
-                raise Exception('Operation cancelled: not enough CPU. Currently using: %.2f%%.' % current)
+    def cancellable_decorator(func):
+        def has_enough_resources(*args, **kwargs):
+            """Has the machine reached the CPU consumption threshold?"""
+            if 'cpu' in check:
+                max_cpu = app.config['MAXIMUM_CPU']
+                current = psutil.cpu_percent(interval=1)  #  It blocks it for a second
+                if current >= max_cpu:
+                    raise Exception('Operation cancelled: not enough CPU. Currently using: %.2f%%.' % current)
 
-        if 'memory' in check:
-            max_memory = app.config['MAXIMUM_MEMORY']
-            current = psutil.virtual_memory().percent
-            if current >= max_memory:
-                raise Exception('Operation cancelled: not enough Memory. Currently using: %.2f%%.' % current)
+            if 'memory' in check:
+                max_memory = app.config['MAXIMUM_MEMORY']
+                current = psutil.virtual_memory().percent
+                if current >= max_memory:
+                    raise Exception('Operation cancelled: not enough Memory. Currently using: %.2f%%.' % current)
 
-        logger.info('All the thresholds were passed.')
-        return func(*args, **kwargs)
-    return has_enough_resources
+            logger.info('All the thresholds were passed.')
+            return func(*args, **kwargs)
+        return has_enough_resources
+    return cancellable_decorator
 
 
 def create_instances(num_containers):
